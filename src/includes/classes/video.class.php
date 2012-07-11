d5a2e1663d128d848a74626a1de4d67c93f1ba02
Leustean Marius <leusteanmarius@yahoo.com>
2012-07-11 23:15:03 +0300

    some methods on video class

--- src/includes/classes/video.class.php
+++ src/includes/classes/video.class.php
@@ -1,9 +1,147 @@
 <?php
 
 
-class video{
+class video {
+	
+	/*
+	 * All class attributes are the same
+	 * with fields in `videos` table
+	 *
+	*/
+	
+	public $id;
+	public $category;
+	public $title;
+	public $slug;
+	public $desc;
+	public $from;
+	public $filename;
+	public $author;
+	public $relative_path;
+	public $extension;
+	public $embed;
+	public $image;
+	public $views;
+	public $featured;
+	
+	
+	/*========================
+	 * video class constructor
+	 *
+	 * Parameters: int $id -> the video id
+	 *
+	 * Description: if the $id parameter is given on class instantiation
+	 *				the object will be fetched with attributes from database
+	 *				where video id is the given parameter
+    */
+	function __construct($id=false){
+		
+		if( $id !== false ){
+		
+			global $mysql;
+			
+			$id = mysql_real_escape_string($id);
+			$q = $mysql->query("SELECT * FROM `videos` WHERE `ID`='$id'");
+			
+			if( $q ){
+				$q = mysql_fetch_assoc($q);
+				foreach($q as $key => $val)
+					$this->$key = $val;	
+				
+			}
+			
+		}
+	}
+	
+	/*=======================
+	 * function sanitize_all()
+	 *
+	 * Description: this private method is called in methods like insert() or update()
+	 *				and prepare all class attributes to be inserted in database
+	 *
+	*/
+	private function sanitize_all(){
+		foreach( get_class_vars($this) as $var => $val )
+			$this->$var = mysql_real_escape_string(strip_tags(htmlspecialchars($val,ENT_QUOTES,'UTF-8')));
+	}
+	
+	
+	/*===================
+	 * function comments()
+	 *
+	 * Returns: an array with comments objects corresponding to this video
+	 *
+	 * Parameters: none
+	 *
+	*/
+	public function comments(){
+		
+		$comments = array();
+		global $mysql;
+		
+		$q = $mysql->query("SELECT * FROM `videos_comments` WHERE`video_id`='{$this->id}'");
+		if( $q ){
+			while($a = mysql_fetch_object($q) )	
+				$comments[] = $a;
+			return $comments;
+		}
+		else 
+			return false;
+	}
+	
+	//TODO
+	/*=======================
+	 * function author()
+	 * 
+	 * Returns: an user object corresponding to this video's author
+	*/	
+	
+	private function author(){
+		//this function returns the user object
+		//but we need user class to do this
+		//so we will wait for our project-mate to finish the users system
+	}
+	
+	/*=================
+	 * function insert()
+	 *
+	 * Description: first set all class attributes
+	 * 				than call this method to insert the new video into database
+	 *
+	*/
+	public function insert(){
+		
+		$this->sanitize_all();
+		
+		global $mysql;
+		$values = ''; $insert = '';
+		
+		foreach( get_class_vars($this) as $var => $val ){
+			if( !empty($val) && $var!='id' ){
+				$values .= "'".mysql_real_escape_string($val)."',";
+				$insert .= "`$var`,";	
+			}
+		}
+		
+		return $mysql->query("INSERT INTO `videos` (".trim($insert,',').") VALUES (".trim($values,',').")");
+	}
+		
+	/*========================
+	 * static function delete()
+	 *
+	 * Parameters: $id -> the video's id you want to delete from db
+	 *
+	 * Description: removes a video from database
+	 *
+	*/
+	public static function delete($id){
+		global $mysql;
+		$id = mysql_real_escape_string($id);
+		return $mysql->query("DELETE FROM `videos` WHERE `ID`='$id'");	
+	}
 
-
+	
+	
 	public function get_youtube_id($youtube_id){	
 		$position       = strpos($youtube_id, 'watch?v=')+8;
         $remove_length  = strlen($youtube_id)-$position;
